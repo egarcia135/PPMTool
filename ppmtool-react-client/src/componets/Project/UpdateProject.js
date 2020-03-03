1c319d726ff7223bb2e58dead7b4aea7b92a87ca
Eduardo <egarci135@gmail.com>
2020-03-03 15:18:51 -0800

    Update Form Validation

--- ppmtool-react-client/src/componets/Project/UpdateProject.js
+++ ppmtool-react-client/src/componets/Project/UpdateProject.js
@@ -16,13 +16,18 @@ class UpdateProject extends Component {
             "projectIdentifier": "",
             "description":"",
             "start_date":"",
-            "end_date": ""
+            "end_date": "",
+            errors: {}
         }
         this.onChange = this.onChange.bind(this)
         this.onSubmit = this.onSubmit.bind(this)
     }
 
     componentWillReceiveProps(nextProps) {
+
+        if(nextProps.errors) {
+            this.setState({errors:nextProps.errors})
+        }
         const {
             id,
             projectName,
@@ -65,6 +70,7 @@ class UpdateProject extends Component {
         this.props.createProject(updateProject, this.props.history)
     }
     render() {
+        const {errors} = this.state
         return (
             <div className="project">
             <div className="container">
@@ -75,16 +81,22 @@ class UpdateProject extends Component {
                         <form onSubmit={this.onSubmit}>
                             <div className="form-group">
                                 <input type="text" 
-                                    className="form-control form-control-lg " 
+                                    className={classnames("form-control form-control-lg", {
+                                        "is-invalid": errors.projectName
+                                    })}
                                     placeholder="Project Name"
                                     name="projectName" 
                                     value={this.state.projectName}
                                     onChange = {this.onChange}
                                 />
+                                {
+                                    errors.projectName && (
+                                    <div className="invalid-feedback">{errors.projectName}</div>
+                                )}
                             </div>
                             <div className="form-group">
                                 <input type="text" 
-                                    className="form-control form-control-lg" 
+                                    className="form-control form-control-lg"
                                     placeholder="Unique Project ID"
                                     name="projectIdentifier"
                                     value={this.state.projectIdentifier}
@@ -94,17 +106,23 @@ class UpdateProject extends Component {
                             </div>
                             <div className="form-group">
                                 <textarea 
-                                    className="form-control form-control-lg" 
+                                    className={classnames("form-control form-control-lg", {
+                                        "is-invalid": errors.description
+                                    })}
                                     placeholder="Project Description"
                                     name="description"
                                     value={this.state.description}
                                     onChange = {this.onChange}
                                 />
+                                {
+                                    errors.description && (
+                                    <div className="invalid-feedback">{errors.description}</div>
+                                )}
                             </div>
                             <h6>Start Date</h6>
                             <div className="form-group">
                                 <input type="date" 
-                                    className="form-control form-control-lg" 
+                                    className="form-control form-control-lg"
                                     name="start_date" 
                                     value={this.state.start_date}
                                     onChange = {this.onChange}
@@ -113,7 +131,7 @@ class UpdateProject extends Component {
                             <h6>Estimated End Date</h6>
                             <div className="form-group">
                                 <input type="date" 
-                                    className="form-control form-control-lg" 
+                                    className="form-control form-control-lg"
                                     name="end_date" 
                                     value={this.state.end_date}
                                     onChange = {this.onChange}
@@ -132,14 +150,17 @@ class UpdateProject extends Component {
 UpdateProject.propTypes = {  
     getProject: PropTypes.func.isRequired,
     createProject: PropTypes.func.isRequired,
-    project: PropTypes.object.isRequired
+    project: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+    
 };
 
 const mapStateToProps = state => ({
-    project: state.project.project
+    project: state.project.project,
+    errors: state.errors
 });
 
 export default connect(
     mapStateToProps, 
     {getProject, createProject}
-    ) (UpdateProject);
+) (UpdateProject);
