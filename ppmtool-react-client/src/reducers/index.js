0bd5bd1098fe6f5ecae3cb93133013e717e6a541
Eduardo <egarci135@gmail.com>
2020-03-11 13:41:54 -0700

    Login returns correct info

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
 import backlog_Reducer from "./backlog_Reducer";
+import securityReducer from "./securityReducer";
 
 
 export default combineReducers ({
     errors: errorReducer,
     project: projectReducer,
-    backlog: backlog_Reducer
+    backlog: backlog_Reducer,
+    security: securityReducer
 
 });
