90c1a7f27687134fb50deff27b1d8aaf694ca85c
Eduardo <egarci135@gmail.com>
2020-03-11 12:22:06 -0700

    Register from Front-end

--- ppmtool-react-client/src/reducers/backlog_Reducer.js
+++ ppmtool-react-client/src/reducers/backlog_Reducer.js
@@ -22,7 +22,7 @@ export default function(state=inititalState, action){
         case DELETE_PROJECT_TASK:
             return {
                 ...state,
-                project_tasks: state.project_tasks.filter(project_task => project_task.projectSequence != action.payload)
+                project_tasks: state.project_tasks.filter(project_task => project_task.projectSequence !== action.payload)
             }
 
 
